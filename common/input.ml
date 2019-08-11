8269ef9e05da6670be6422c562f5a5cb9a78a7e4
Stephane Glondu <steph@glondu.net>
2019-08-11 17:04:27 +0200

    Compile with -safe-string
    Bug-Debian: https://bugs.debian.org/934347

--- common/input.ml
+++ common/input.ml
@@ -44,10 +44,10 @@ let bzip_open_file file =
   (* workaround to avoid segfault :
    * http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=602170 *)
   let _ = Bz2.version in 
-  let s = " " in
+  let s = Bytes.create 1 in
   let ch = Bz2.open_in (open_in file) in
   let input_char ch = 
-    try ignore (Bz2.read ch s 0 1) ; s.[0]
+    try ignore (Bz2.read ch s 0 1) ; Bytes.get s 0
     with End_of_file -> raise IO.No_more_input
   in
   let read ch s pos len =
